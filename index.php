c339e1c780c7ac8aa214121be06d6469e93489f2
Vitalik <LorencoDark10@gmail.com>
2022-11-19 20:01:51 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,93 @@
+<?php
+header('Content-type: text/plain');
+
+/*interface Spotsman{
+	public function run();
+	public function gym();
+	public function eat();
+}*/
+
+/*abstract*/ /*Class Person implements Spotsman{
+	public static $name;*/
+	/*public $job;
+	public $age;
+	public $isMarried;*/
+
+	/*public static function setSum __construct ($name, $age, $job){*/
+		/*$this->name = $name;
+		$this->age = $age;
+		$this->job = $job;*/
+/*		echo $age += $job;
+		self::$name = $name;
+	}
+	public function run(){
+		echo 'im running';
+	}
+
+	public function gym(){
+		echo 'im gym';
+	}
+
+	public function eat(){
+		echo 'im eat';
+	}*/
+/*
+	public function __construct($name, $age, $hobby){
+		echo 'Constructor!!!!!';
+		$this->name = $name;
+		$this->age = $age;
+		$this->hobby = $hobby;
+	}*/
+
+/*public function work()
+{
+	echo "string";
+}*/
+
+//}
+
+/*class Developer extends Person{
+
+public $timeperweek;
+
+public function __construct($name, $age, $job, $timeperweek)
+{
+	parent::__construct($name, $age, $job);
+	$this->timeperweek = $timeperweek;
+}
+
+}*/
+
+//$person = new Person("Victor", 20, 'swim');
+/*$person = new Developer('Victor', 20, "back-end", 20);
+echo $person->name;
+*/
+/*Person::setSum("Vasya", 10, 20);
+echo Person::$name;*/
+
+/*Class Person{
+    public $name = 'Victor';
+    public $age = 20;
+    public $isMarried = true;
+
+    public function __construct($name, $age, $isMarried)
+    {
+        $this->name = $name."\n";
+        $this->age = $age;
+        $this->isMarried = $isMarried;
+    }
+
+    public function sayHello(){
+        return "Pirivet";
+    }
+}
+
+Class Maneger extends Person {
+    public function ya(){
+        echo 'dsf';
+    }
+}
+
+$person = new Maneger('sadfsda',20,true);
+echo $person->sayHello();*/
+?>
